19c748609624e51a4c955a0e6d013769207140c2
Deysi <sude.90@gmail.com>
2017-09-18 12:18:12 -0500

    xxxx

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 var preguntas= [
             'Which is the oldest airline in the world?',
             'TWhat is the longest distance cycling backwards?',
-            'What is the highest speed ever reached by a school bus?,
+            'What is the highest speed ever reached by a school bus?',
             'What is the longest car trip on one tank of gas?'
            ]
 
@@ -12,48 +12,39 @@ var respuestas=[
 ['590 km/h','320 km/h','245 km/h']
 ]
 function jugar(){
-      var indice_aleatorio = Math.floor(Math.random()*preguntas.length);
+      var aleatorio = Math.floor(Math.random()*preguntas.length);
 
 
-    var respuestas_posibles = respuestas[indice_aleatorio];
+    var respuestas = respuestas[aleatorio];
 
 
     var posiciones = [0,1,2];
-    var respuestas_reordenadas = [];
+    var respuestas2 = [];
 
-    var ya_se_metio = false;
-    for(i in respuestas_posibles){
+    var correctas = false;
+    for(i in respuestas){
       var posicion_aleatoria = Math.floor(Math.random()*posiciones.length);
-      if(posicion_aleatoria==0 && ya_se_metio == false){
+      if(posicion_aleatoria==0 && correctas == false){
         indicie_respuesta_correcta =i;
-        ya_se_metio = true;
+        correctas = true;
       }
-      respuestas_reordenadas[i] = respuestas_posibles[posiciones[posicion_aleatoria]];
+      respuestas2[i] = respuestas[posiciones[posicion_aleatoria]];
       posiciones.splice(posicion_aleatoria, 1);
     }
 
     var txt_respuestas="";
-    for(i in respuestas_reordenadas){
-      txt_respuestas += '<input type="button" name="pp" value="'+i+'">'+respuestas_reordenadas[i]+'<br>';
+    for(i in respuestas2){
+      txt_respuestas += '<input type="button" name="pp" value="'+i+'">'+respuestas2[i]+'<br>';
     }
 
     document.getElementById("respuestas").innerHTML = txt_respuestas;
-    document.getElementById("preguntas").innerHTML = preguntas[indice_aleatorio];
+    document.getElementById("preguntas").innerHTML = preguntas[aleatorio];
 
 }
 function comprobar(){
-  var respuesta = $("input[type=radio]:checked").val();
+  var respuesta = $("input[type=button]").val();
 
   //alert(respuesta);
-  //alert(indicie_respuesta_correcta);
-  if(respuesta ==indicie_respuesta_correcta){
-    alert("que bien!");
-  }else{
-    alert("me tiro por un puente");
-  }
-  jugar();
+  alert(indicie_respuesta_correcta);
+    jugar();
 }
-  </script>
-
-</body>
-</html>
